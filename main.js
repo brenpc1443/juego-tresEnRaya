e6ecc1f3dd3509d391c66a3ea2e0765bd50b624a
Bren pc <brayanp.cabrera@gmail.com>
2022-12-28 13:21:31 -0500

    se culminó el juego, en la parte más fácil, se dividio n do jugadores, y este solo puede jugar una sola vez, y se puede cambiar de turno y todo, aún falta agregar una que otra función para hacer el juego mñas intuitivo

--- main.js
+++ main.js
@@ -7,4 +7,41 @@ const item6 = document.getElementById("item6");
 const item7 = document.getElementById("item7");
 const item8 = document.getElementById("item8");
 const item9 = document.getElementById("item9");
+const btn   = document.querySelector (".btn" );
 
+item1.addEventListener("click",() => miTurno(item1));
+item2.addEventListener("click",() => miTurno(item2));
+item3.addEventListener("click",() => miTurno(item3));
+item4.addEventListener("click",() => miTurno(item4));
+item5.addEventListener("click",() => miTurno(item5));
+item6.addEventListener("click",() => miTurno(item6));
+item7.addEventListener("click",() => miTurno(item7));
+item8.addEventListener("click",() => miTurno(item8));
+item9.addEventListener("click",() => miTurno(item9));
+btn.  addEventListener("click", jugador);
+
+let x = 0;
+let y = 0;
+function jugador(){
+    y = 0;
+    if ( x == 0){
+        x++;
+    }else{
+        x = 0;
+    }
+}
+
+function miTurno (item){
+    if ( y == 0){
+        switch(x){
+            case 0: 
+                item.innerText = "X";
+                y = 1;
+                break;
+            case 1:
+                item.innerText = "O";
+                y = 1;
+                break;
+        }
+    }
+}
