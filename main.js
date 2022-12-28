f352230a5dfc9918330c3b542dc51a676a377fcb
Bren pc <brayanp.cabrera@gmail.com>
2022-12-28 12:10:48 -0500

    se crearon tres archivos, html, css, js, para realizar el proyecto, se les colocó su código base, en los tres archivos

---
+++ main.js
@@ -0,0 +1,10 @@
+const item1 = document.getElementById("item1");
+const item2 = document.getElementById("item2");
+const item3 = document.getElementById("item3");
+const item4 = document.getElementById("item4");
+const item5 = document.getElementById("item5");
+const item6 = document.getElementById("item6");
+const item7 = document.getElementById("item7");
+const item8 = document.getElementById("item8");
+const item9 = document.getElementById("item9");
+
